77dc65dc90210e4ec9b46dde327c06f11148c599
SHUANESH <shuanesh1817@gmail.com>
2021-11-25 14:13:53 +0200

    gh-pages update

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "core-js/fn/object/assign";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/Main";
-// import SimpleReactLightbox from 'simple-react-lightbox';
-
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById("app"));
